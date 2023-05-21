24d64c240455dd5afef678efc84929613e2f97a3
Pedro Silva <pedronicks25@gmail.com>
2023-05-20 22:27:57 -0300

    pagina de cardágio deletada, adição de CSS

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -126,5 +129,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
--- restaurante/urls.py
+++ restaurante/urls.py
@@ -4,13 +4,11 @@ from core import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('cadastro-cliente/', views.cadastro_cliente, name='cadastro_cliente'),
     path('reserva/', views.reserva, name='reserva'),
     path('delivery/', views.delivery, name='delivery'),
-    path('cardapio/', views.cardapio, name='cardapio'),
     path('delivery/<int:pedido_id>/confirmar/', views.confirmar_delivery, name='confirmar_delivery'),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
