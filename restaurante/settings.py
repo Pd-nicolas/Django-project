baab8591e9a6671854ebfb1ff68c2333afda3b4c
Pd-nicolas <104381007+Pd-nicolas@users.noreply.github.com>
2023-05-31 14:48:23 -0300

    Update settings.py

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -1,5 +1,6 @@
 import os
 
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 """
 Django settings for restaurante project.
@@ -119,23 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
-
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 
 
