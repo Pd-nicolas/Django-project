38fe550a2cf42b3f79cd1d3c8c7cbddff9e27845
Pd-nicolas <104381007+Pd-nicolas@users.noreply.github.com>
2023-05-31 14:54:16 -0300

    Update settings.py

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -134,6 +134,16 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 class CustomStaticFilesHandler(StaticFilesHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
