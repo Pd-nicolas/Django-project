4699b05a014ad56eea74967efccbceb839adfaa9
Pd-nicolas <104381007+Pd-nicolas@users.noreply.github.com>
2023-05-31 13:01:48 -0300

    Update settings.py

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -134,8 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR.parent, 'static')
 ]
+
 
 
 
