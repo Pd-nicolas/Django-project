7a8a4bf35866c2fbf7ceb449be836ac83f552179
Pd-nicolas <104381007+Pd-nicolas@users.noreply.github.com>
2023-05-31 14:20:38 -0300

    Update wsgi.py

--- restaurante/wsgi.py
+++ restaurante/wsgi.py
@@ -9,7 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+settings.DEBUG = False
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurante.settings')
 
