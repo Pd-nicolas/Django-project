9240e9f2f9bb7c8f8bf835596a3d8b795304793f
Pedro Silva <pedronicks25@gmail.com>
2023-05-31 00:22:03 -0300

    ultimo

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7%f6ff6k*6aaz@@8ik_^&9mesrikpb_1uv=4^ndl!d37s_+hh(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
